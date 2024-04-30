bc38b228a8eb79e566361ac2a7863c7d5c1a6891
Ross <Ross.wilson789@gmail.com>
2024-04-30 14:12:26 +0100

    Added Wide-Fov Lidar ToF code

--- freeRTOS_skeleton/skelly_bones_two_IMU/skelly_bones_two_IMU.ino
+++ freeRTOS_skeleton/skelly_bones_two_IMU/skelly_bones_two_IMU.ino
@@ -4,9 +4,27 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
-
 #include <ESP32Servo.h>
+#include <Arduino.h>
+#include <vl53l5cx_class.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+#include <stdlib.h>
 
+#define SerialPort Serial
+
+#define I2C_SDA 1
+#define I2C_SCL 2
+
+#define LPN_PIN 3
+#define I2C_RST_PIN 4
+#define PWREN_PIN 5
+
+#define refelction_threshold 50
+#define distance_threshold 200
 //#include <customLib.h>
 
 
@@ -24,7 +42,19 @@ Servo steerServo;
 
 float heading=0.0;
 
+char temp_str[20];
+typedef struct
+{
+    int16_t target_distance[(VL53L5CX_RESOLUTION_8X8*VL53L5CX_NB_TARGET_PER_ZONE)];
+    float target_angle[(VL53L5CX_RESOLUTION_8X8*VL53L5CX_NB_TARGET_PER_ZONE)];
+    int16_t target_reflectance[(VL53L5CX_RESOLUTION_8X8*VL53L5CX_NB_TARGET_PER_ZONE)];
 
+} Valid_target_Info;
+
+Valid_target_Info target_info;
+
+// Components.
+VL53L5CX sensor_vl53l5cx_sat(&Wire, LPN_PIN, I2C_RST_PIN);
 
 void setup() { 
   Serial.begin(115200);
@@ -109,19 +139,155 @@ void initial_drive (void*pvParameters){
 void sensor_start(void * pvParameters){                                                     //Start the TOF sensors
 
     if (ulTaskNotifyTake(pdTRUE, portMAX_DELAY )!=0){
-      int i=0;
       
+      // Enable PWREN pin if present
+      if (PWREN_PIN >= 0) {
+        pinMode(PWREN_PIN, OUTPUT);
+        digitalWrite(PWREN_PIN, HIGH);
+        delay(10);
+      }
 
+      // Initialize serial for output.
+      SerialPort.begin(115200);
+      delay(1000);
+      SerialPort.println("Initialize... Please wait, it may take few seconds...");
 
+      // Initialize I2C bus.
+      Wire.begin(I2C_SDA, I2C_SCL);
 
+      VL53L5CX_ResultsData Results;
+      uint8_t NewDataReady = 0;
+      char temp_str[64];
+      uint8_t status, i;
 
-      while(1){  
+      uint16_t                closest_white_distance = 9999, closest_black_distance = 9999;
+      float                   closest_white_angle = 0, closest_black_angle = 0;
+
+      // Configure VL53L5CX satellite component.
+      sensor_vl53l5cx_sat.begin();
+
+      sensor_vl53l5cx_sat.init_sensor();
+
+      //Enter 8X8 mode
+      status = sensor_vl53l5cx_sat.vl53l5cx_set_resolution(VL53L5CX_RESOLUTION_8X8);
+      if (status) {
+        snprintf(temp_str, sizeof(temp_str), "vl53l5cx_set_resolution failed, status %u\r\n", status);
+        SerialPort.print(temp_str);
+      }
+
+      //Enter 15 hz mode
+      status = sensor_vl53l5cx_sat.vl53l5cx_set_ranging_frequency_hz(1);
+      if (status) {
+        snprintf(temp_str, sizeof(temp_str), "vl53l5cx_set_ranging_frequency_hz failed, status %u\r\n", status);
+        SerialPort.print(temp_str);
+      }
+
+      // Start Measurements
+      sensor_vl53l5cx_sat.vl53l5cx_start_ranging();
+      
+      while(1)
+      {
+        do 
+        {
+            status = sensor_vl53l5cx_sat.vl53l5cx_check_data_ready(&NewDataReady);
+        } while (!NewDataReady);
+
+          if ((!status) && (NewDataReady != 0)) 
+          {
+            status = sensor_vl53l5cx_sat.vl53l5cx_get_ranging_data(&Results);
+
+            for(i = 0; i < 64; i++)
+            {
+                //Check data is valid if there is 1 target and status is 5. If measurment is invalid, set zone to 9999.
+                if(Results.target_status[VL53L5CX_NB_TARGET_PER_ZONE*i] != 5 && Results.target_status[VL53L5CX_NB_TARGET_PER_ZONE*i] != 6 && Results.target_status[VL53L5CX_NB_TARGET_PER_ZONE*i] != 9 && Results.nb_target_detected[VL53L5CX_RESOLUTION_8X8] != 1)
+                {
+                    target_info.target_distance[i] = 9999;
+                    target_info.target_reflectance[i] = 9999;
+                }
+                else
+                {
+                    target_info.target_distance[i] = Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE*i];
+                    target_info.target_reflectance[i] = Results.reflectance[VL53L5CX_NB_TARGET_PER_ZONE*i];
+                }
+
+                //assign each zone an angle. probably an easier way to do this but i dont know it
+                if(i % 8 == 0){target_info.target_angle[i] = -19.6875;}
+                else if((i - 1) % 8 == 0){target_info.target_angle[i] = -14.0626;}
+                else if((i - 2) % 8 == 0){target_info.target_angle[i] = -8.4375;}
+                else if((i - 3) % 8 == 0){target_info.target_angle[i] = -2.8125;}
+                else if((i - 4) % 8 == 0){target_info.target_angle[i] = 2.8125;}
+                else if((i - 5) % 8 == 0){target_info.target_angle[i] = 8.4375;}
+                else if((i - 6) % 8 == 0){target_info.target_angle[i] = 14.0626;}
+                else if((i - 7) % 8 == 0){target_info.target_angle[i] = 19.6875;}
+            }
+
+            //print validated distance for middle 4 rows
+            for(i = 16; i < 48; i++)
+            {
+                if(i % 8 == 0)
+                {
+                    SerialPort.print("\n");
+                }
+                snprintf(temp_str, 20, "%04d", target_info.target_distance[i]);
+                SerialPort.print(temp_str);
+                
+                SerialPort.print("\t");
+            }
+            //Print middle angles of zones
+            for(i = 16; i < 48; i++)
+            {
+                if(i % 8 == 0)
+                {
+                    SerialPort.print("\n");
+                }
+                snprintf(temp_str, 20, "%04e", target_info.target_angle[i]);
+                SerialPort.print(temp_str);
+                
+                SerialPort.print("\t");
+            }
+            //Print target reflectances
+            for(i = 16; i < 48; i++)
+            {
+                
+                if(i % 8 == 0)
+                {
+                    SerialPort.print("\n");
+                }
+                snprintf(temp_str, 20, "%04d", target_info.target_reflectance[i]);
+                SerialPort.print(temp_str);
+                
+                SerialPort.print("\t");
+            }
+
+            closest_white_distance = 9999; //reset closest white distance from last iteration. 
+            closest_black_distance = 9999; //reset closest black distance from last iteration. 
+
+            for(i = 16; i < 48; i++) //find closest white and black zone's distances and angles
+            {
+                //find closest white zone
+                if(target_info.target_distance[i] < closest_white_distance && target_info.target_reflectance[i] > refelction_threshold)
+                {
+                    closest_white_distance = target_info.target_distance[i];
+                    closest_white_angle = target_info.target_angle[i];
+                }
+                //find closest black zone
+                if(target_info.target_distance[i] < closest_black_distance && target_info.target_reflectance[i] < refelction_threshold)
+                {
+                    closest_black_distance = target_info.target_distance[i];
+                    closest_black_angle = target_info.target_angle[i];
+                }
+            }
+            SerialPort.print("\n");
+            SerialPort.print("\n");
+        }
+        delay(10);
+  
         sensors_event_t a,g,temp;
         mpu.getEvent(&a,&g,&temp);                         
       
-        if (i<20){
+        if (i<20)
+        {
           offset_error+=g.gyro.x;
-          
         }
         i++;
         offset_error=offset_error/20;
